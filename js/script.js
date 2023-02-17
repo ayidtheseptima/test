0e4a080342c4699f787d448103e62a46cb266ced
ayidtheseptima <93842487+ayidtheseptima@users.noreply.github.com>
2023-02-17 22:30:39 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  grabCursor: true,
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  freeMode: true,
+});
